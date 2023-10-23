f022a5634651051c6dc5ea504ca040f792987948
verrelprayogii2@gmail.com <verrelprayogii2@gmail.com>
2023-10-23 16:37:56 +0800

    test

--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.post('/', productController.createProduct); // Handle POST requests to /
+router.post('/', productController.createProduct);
 
 module.exports = router;
